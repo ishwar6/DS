f9abbf5e04f84efe61af9d07de4e81833d836305
ishwar6 <iisudrj11@gmail.com>
2022-01-29 12:58:48 +0530

    strike

--- Number theory/gcd_lcm.py
+++ Number theory/gcd_lcm.py
@@ -74,3 +74,51 @@ def lcm(a, b):
 
 
 #print(lcm(2, 0))
+
+
+def strike(text):
+    """ Renders string with strike-through characters through it.
+
+        `strike('hello world')` -> '̶h̶e̶l̶l̶o̶ ̶w̶o̶r̶l̶d'
+
+        Notes
+        -----
+        \u0336 is a special strike-through unicode character; it
+        is not unique to Python."""
+    return ''.join('\u0336{}'.format(c) for c in text)
+
+
+# a = strike("aadfdaf")
+# print(a)
+
+
+class ShoppingList:
+    def __init__(self, items):
+        self._needed = set(items)
+        self._purchased = set()
+
+    def __repr__(self):
+        """ Returns formatted shopping list as a string with
+            purchased items being crossed out.
+
+            Returns
+            -------
+            str"""
+        if self._needed or self._purchased:
+            remaining_items = [str(i) for i in self._needed]
+            purchased_items = [strike(str(i)) for i in self._purchased]
+            # You wont find the • character on your keyboard. I simply
+            # googled "unicode bullet point" and copied/pasted it here.
+            return "• " + "\n• ".join(remaining_items + purchased_items)
+
+    def add_new_items(self, items):
+        self._needed.update(items)
+
+    def mark_purchased_items(self, items):
+        self._purchased.update(set(items) & self._needed)
+        self._needed.difference_update(self._purchased)
+
+
+l = ShoppingList(["grapes", "beets", "apples", "milk", "melon", "coffee"])
+l.mark_purchased_items(["grapes", "beets", "milk"])
+print(l)
