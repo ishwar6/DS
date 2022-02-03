5f28eba56a3518c95c76eab67082b1f6df3e2bd0
ishwar6 <iisudrj11@gmail.com>
2022-02-03 15:21:46 +0530

    hashmap

--- Sliding Window/no_repeat_longest.py
+++ Sliding Window/no_repeat_longest.py
@@ -1,5 +1,9 @@
 # Given a string, find the length of the longest substring which has no repeating characters.
 
+# We can use a HashMap to remember the last index of each character we have processed.
+#  Whenever we get a repeating character we will shrink our sliding window
+#  to ensure that we always have distinct characters in the sliding window.
+
 
 # aabcbd => abc (3)
 def longest_substring(str):
@@ -32,7 +36,7 @@ def longest_substring(str):
     return max_length
 
 
-longest_substring("aaasdpfeasdfaaa")  # 6 asdpfe
+# longest_substring("aaasdpfeasdfaaa")  # 6 asdpfe
 
 # {'a': 0}
 # MAX 0 0 1
@@ -46,3 +50,29 @@ longest_substring("aaasdpfeasdfaaa")  # 6 asdpfe
 # {'a': 12, 's': 9, 'd': 10, 'p': 5, 'f': 11, 'e': 7}
 # {'a': 13, 's': 9, 'd': 10, 'p': 5, 'f': 11, 'e': 7}
 # 6 asdpfe
+
+
+# The time complexity of the above algorithm will be O(N) where ‘N’ is the number of characters in the input string.
+# Space complexity is O(1): Max 26 elements can come in hashmap
+#  we can use a fixed-size array instead of the HashMap.
+
+def non_repeat(str):
+    window_start = 0
+    max_length = 0
+    char_index_map = {}
+
+  # try to extend the range [windowStart, windowEnd]
+    for window_end in range(len(str)):
+        right_char = str[window_end]
+    # if the map already contains the 'right_char', shrink the window from the beginning so that
+        print(char_index_map, right_char)
+        if right_char in char_index_map:
+            print("INSIDE", window_start, char_index_map[right_char]+1)
+            window_start = max(window_start, char_index_map[right_char]+1)
+        char_index_map[right_char] = window_end
+        max_length = max(max_length, window_end-window_start+1)
+    return max_length
+
+
+a = non_repeat("aabcas")
+print(a)
