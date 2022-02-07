f8cdf0fa0c5711a9315037422c8ad5cc499e25c3
ishwar6 <iisudrj11@gmail.com>
2022-02-07 11:38:57 +0530

    fib

--- Recursion/basic.py
+++ Recursion/basic.py
@@ -1,4 +1,7 @@
 
+from timeit import Timer
+
+
 def print_r(n):
     if(n == 1):
         print(1)
@@ -7,19 +10,32 @@ def print_r(n):
     print_r(n-1)
 
 
-# print_r(4)
-
-
-def print_r(n):
-   # print("UP for", n)
-    if(n == 1):
-        print(1)
+def fib(n):
+    """ recursive version of the Fibonacci function """
+    if n == 0:
+        return 0
+    elif n == 1:
         return 1
-
-    print_r(n-1)
-    print("here for", n)
-
-    print_r(n-1)
+    else:
+        return fib(n-1) + fib(n-2)
 
 
-print_r(3)
+def fibi(n):
+    """ iterative version of the Fibonacci function """
+    old, new = 0, 1
+    if n == 0:
+        return 0
+    for i in range(n-1):
+        old, new = new, old + new
+    return new
+
+
+memo = {0: 0, 1: 1}
+
+
+def fibm(n):
+    """ recursive Fibonacci function which memoizes previously 
+    calculated values with the help of a dictionary memo"""
+    if not n in memo:
+        memo[n] = fibm(n-1) + fibm(n-2)
+    return memo[n]
