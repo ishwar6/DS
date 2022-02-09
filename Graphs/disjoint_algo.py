35c8fc93d1b04727ab71ba744314e177cc0ba4f4
ishwar6 <iisudrj11@gmail.com>
2022-02-09 15:09:55 +0530

    test

--- Graphs/disjoint_algo.py
+++ Graphs/disjoint_algo.py
@@ -128,18 +128,19 @@ class UnionFind:
         return self.find(x) == self.find(y)
 
 
-# Test Case
-uf = UnionFind(10)
-# 1-2-5-6-7 3-8-9 4
-uf.union(1, 2)
-uf.union(2, 5)
-uf.union(5, 6)
-uf.union(6, 7)
-uf.union(3, 8)
-uf.union(8, 9)
-print(uf.connected(1, 5))  # true
-print(uf.connected(5, 7))  # true
-print(uf.connected(4, 9))  # false
-# 1-2-5-6-7 3-8-9-4
-uf.union(9, 4)
-print(uf.connected(4, 9))  # true
+def test_rank():
+    # Test Case
+    uf = UnionFind(10)
+    # 1-2-5-6-7 3-8-9 4
+    uf.union(1, 2)
+    uf.union(2, 5)
+    uf.union(5, 6)
+    uf.union(6, 7)
+    uf.union(3, 8)
+    uf.union(8, 9)
+    print(uf.connected(1, 5))  # true
+    print(uf.connected(5, 7))  # true
+    print(uf.connected(4, 9))  # false
+    # 1-2-5-6-7 3-8-9-4
+    uf.union(9, 4)
+    print(uf.connected(4, 9))  # true
