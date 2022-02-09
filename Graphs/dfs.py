f1cbfe54232ce634dc5f1db3b9d30b26760e2841
ishwar6 <iisudrj11@gmail.com>
2022-02-09 20:36:09 +0530

    decorators

--- Graphs/dfs.py
+++ Graphs/dfs.py
@@ -7,6 +7,63 @@
 
 
 # defaultdict
-
 from collections import defaultdict
-a = defaultdict(list)
+
+# Generate adjacency list for undirected graph
+
+
+def generateAdjacencyLst(edges):
+    # it will be an empty list if for any key there exists no value
+    adjacencyList = defaultdict(list)
+    for u, v in edges:
+        # print(u, v)
+        # print(adjacencyList)
+        adjacencyList[u].append(v)
+        adjacencyList[v].append(u)
+    return adjacencyList
+
+
+edges = [['A', 'B'], ['A', 'C'], ['B', 'D'], ['B', 'E'], ['C', 'F'], ['C', 'G'], [
+    'D', 'H'], ['D', 'I'], ['E', 'J'], ['E', 'K'], ['F', 'L'], ['F', 'M']]
+adjacencyList = generateAdjacencyLst(edges)
+print(adjacencyList)
+
+# defaultdict(<class 'list'>,
+# {'A': ['B', 'C'],
+# 'B': ['A', 'D', 'E'],
+#  'C': ['A', 'F', 'G'],
+#  'D': ['B', 'H', 'I'],
+# 'E': ['B', 'J', 'K'],
+# 'F': ['C', 'L', 'M'],
+# 'G': ['C'],
+#  'H': ['D'],
+#  'I': ['D'],
+#   'J': ['E'],
+#   'K': ['E'],
+#    'L': ['F'],
+#    'M': ['F']}
+#    )
+
+# find dfs traversal from starting vertex
+
+
+def dfs(adjacencyList, vertex, visitedSet=None, path=None):
+    # create memo once in top-level call
+    if visitedSet is None:
+        visitedSet = set()
+    if path is None:
+        path = []
+
+    visitedSet.add(vertex)
+    path.append(vertex)
+    print(visitedSet, "PATH", path)
+    if vertex in adjacencyList:
+        print("IN", vertex, adjacencyList[vertex])
+        for neighbor in adjacencyList[vertex]:
+            if neighbor not in visitedSet:
+                print("calling dfs for", neighbor)
+                dfs(adjacencyList, neighbor, visitedSet, path)
+    return path
+
+
+print(dfs(adjacencyList, 'A'))
