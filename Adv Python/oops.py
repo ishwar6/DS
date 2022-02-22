7255a6b39a58645317926e01621897e718075413
ishwar6 <iisudrj11@gmail.com>
2022-02-22 12:10:01 +0530

    aggregation

--- Adv Python/oops.py
+++ Adv Python/oops.py
@@ -23,3 +23,33 @@ print(p1.calculate_total_price)
 # all functions in a class are by default bound functions.
 # The instance obj is automatically passed as the first argument  to the function called
 # and hence the first parameter of the function will be used to access the variables/functions of the object.
+
+
+class Salary:
+    def __init__(self, basicPay, flexiPay, bonus, PPF):
+        self.basicPay = basicPay
+        self.flexiPay = flexiPay
+        self.bonus = bonus
+        self.PPF = PPF
+
+    def get_ctc(self):
+        return ((self.basicPay+self.flexiPay)*12+self.bonus+self.PPF)
+
+
+class Employee:
+    def __init__(self, name, empId, salaryObj):  # The salary object is passed
+        self.name = name
+        self.id = empId
+        self.salObj = salaryObj  # The salary object is instantiated
+
+    def annualCTC(self):
+        # Calling the get_ctc function of salary class
+        return "Total salary for " + str(self.name) + " with Employee ID " + str(self.id) + " is: " + str(self.salObj.get_ctc())
+
+
+obj_sal1 = Salary(600, 1000, 10000, 3000)
+obj_emp1 = Employee("Employee 1", "270991", obj_sal1)
+print(obj_emp1.annualCTC())
+obj_sal2 = Salary(600, 1000, 0, 3000)
+obj_emp2 = Employee("Employee 2", "270919", obj_sal2)
+print(obj_emp2.annualCTC())
