bcd9110a1c947f928abf3930089698e6610cb305
ishwar6 <iisudrj11@gmail.com>
2022-02-28 13:03:30 +0530

    threading

---
+++ Adv Python/threading.py
@@ -0,0 +1,30 @@
+from threading import *
+
+
+def display():
+    for i in range(10):
+        print("child thread")
+
+
+t = Thread(target=display)
+t.start()
+
+
+def display_p():
+    for i in range(10):
+        print("main thread")
+
+
+display_p()
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n-1)+fib(n-2)
+
+
+for i in range(10):
+    print(fib(i))
