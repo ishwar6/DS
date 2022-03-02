9b6c5b0261e890d5694d4737d6765f32552a906d
ishwar6 <iisudrj11@gmail.com>
2022-03-02 11:16:08 +0530

    Vision

--- Adv Python/threadinga.py
+++ Adv Python/threadinga.py
@@ -1,87 +1,41 @@
-import asyncio
-from threading import *
+# A1, A2, A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15]
+# Output – [A1,A15,A2,A3,A14,A4,A5,A6,A13,A7,A8,A9,A10,A12,A11].
+# Sample Input – [2,5,9,10,45,2,34,13,49,20,].
+# Output – [2,20,5,9,49,10,45,2,13,34].
+
+def list_print(mylist):
+    l, r = 1, len(mylist)
+    res = []
+    prev = 0
+    res.append(mylist[0])
+
+    for i in range(1, len(mylist)):
+        r -= 1
+        if len(res) <= len(mylist):
+            res.append(mylist[r])
+
+        # increment l by i units
+        prev = l
+        l = l + i+1
+        # print(prev, l)
+        if l < len(mylist) and len(res) < len(mylist):
+            for k in range(prev, l):
+                res.append(mylist[k])
+
+        # decrease j by one unit
+    return res
 
 
-def display():
-    for i in range(10):
-        print("child thread")
+a = list_print(list(range(1, 31)))
+print(a)
 
 
-t = Thread(target=display)
-# t.start()
+Msg(Msg_id, user_id, text, file, timestamp)
 
+OneToOneChat(user_id_1, user_id_2, timestamp, Msg_id)
 
-def display_p():
-    for i in range(10):
-        print("main thread")
+GroupChat(GroupChat_id, list_of_user_ids, host_user_id, Msg_id, timestamp)
 
+State(User_id, GroupChat_id, is_active, is_muted)
 
-# display_p()
-
-
-def fib(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fib(n-1)+fib(n-2)
-
-
-async def main():
-    print('hi')
-    t = asyncio.create_task(foo())
-
-    print("finished")
-    await t
-
-
-async def foo():
-    await asyncio.sleep(5)
-    print("foooo")
-
-
-# asyncio.run(main())
-
-# import math
-# l,r = 0,0
-# window_sum = 0
-# min_length = math.inf
-
-# for r in range(len(A)):
-#     window_sum = window_sum + A[r]
-
-#     while window_sum >= target_sum:
-#         min_length = min(min_length, r-l+1)
-#         window_sum = window_sum - A[l]
-#         l = l-1
-
-# #check condition if min_length never get updated
-# if min_length == math.inf:
-#     return 0
-# return min_length
-
-
-def k_distinct(A: str, k: int):
-    l, r, window_sum, max_l = 0, 0, 0, 0
-    hash_dict = {}
-
-    for r in range(len(A)):
-        current_char = A[r]
-        hash_dict[current_char] = hash_dict.get(current_char, 0) + 1
-
-        while len(hash_dict) > k:
-            left_char = A[l]
-            hash_dict[left_char] -= 1
-            l += 1
-            if hash_dict[left_char] == 0:
-                del hash_dict[left_char]
-        print(max_l, l, r)
-        max_l = max(max_l, r-l+1)
-    return max_l
-
-
-# a = k_distinct("cbbebi", 3)
-# print(a)
-
-stats = {'a': 1000, 'b': 3000, 'c': 100}
-print(max(stats))
+GroupMsg(GroupChat_id, Msg_id)
