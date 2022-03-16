a3f21a859ce431064b7038f8976b4007cdb9a30d
ishwar6 <iisudrj11@gmail.com>
2022-03-16 22:01:05 +0530

    abc

--- .vscode/SD/park.py
+++ .vscode/SD/park.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from abc import ABC, abstractmethod
 
 
 class VehicleType(Enum):
@@ -6,29 +7,115 @@ class VehicleType(Enum):
 
 
 class ParkingSpotType(Enum):
-  HANDICAPPED, COMPACT, LARGE, MOTORBIKE, ELECTRIC = 1, 2, 3, 4, 5
+    HANDICAPPED, COMPACT, LARGE, MOTORBIKE, ELECTRIC = 1, 2, 3, 4, 5
 
 
 class AccountStatus(Enum):
-  ACTIVE, BLOCKED, BANNED, COMPROMISED, ARCHIVED, UNKNOWN = 1, 2, 3, 4, 5, 6
+    ACTIVE, BLOCKED, BANNED, COMPROMISED, ARCHIVED, UNKNOWN = 1, 2, 3, 4, 5, 6
 
 
 class ParkingTicketStatus(Enum):
-  ACTIVE, PAID, LOST = 1, 2, 3
+    ACTIVE, PAID, LOST = 1, 2, 3
 
 
 class Address:
-  def __init__(self, street, city, state, zip_code, country):
-    self.__street_address = street
-    self.__city = city
-    self.__state = state
-    self.__zip_code = zip_code
-    self.__country = country
+    def __init__(self, street, city, state, zip_code, country):
+        self.__street_address = street
+        self.__city = city
+        self.__state = state
+        self.__zip_code = zip_code
+        self.__country = country
 
 
 class Person():
-  def __init__(self, name, address, email, phone):
-    self.__name = name
-    self.__address = address
-    self.__email = email
-    self.__phone = phone
+    def __init__(self, name, address, email, phone):
+        self.__name = name
+        self.__address = address
+        self.__email = email
+        self.__phone = phone
+
+
+class Account:
+    def __init__(self, user_name, password, person, status=AccountStatus.Active):
+        self.__user_name = user_name
+        self.__password = password
+        self.__person = person
+        self.__status = status
+
+    def reset_password(self):
+        None
+
+
+class Admin(Account):
+    def __init__(self, user_name, password, person, status=AccountStatus.Active):
+        super().__init__(user_name, password, person, status)
+
+    def add_parking_floor(self, floor):
+        None
+
+    def add_parking_spot(self, floor_name, spot):
+        None
+
+    def add_parking_display_board(self, floor_name, display_board):
+        None
+
+    def add_customer_info_panel(self, floor_name, info_panel):
+        None
+
+    def add_entrance_panel(self, entrance_panel):
+        None
+
+    def add_exit_panel(self, exit_panel):
+        None
+
+
+class ParkingAttendant(Account):
+    def __init__(self, user_name, password, person, status=AccountStatus.Active):
+        super().__init__(user_name, password, person, status)
+
+    def process_ticket(self, ticket_number):
+        None
+
+
+class ParkingSpot(ABC):
+    def __init__(self, number, parking_spot_type):
+        self.__number = number
+        self.__free = True
+        self.__vehicle = None
+        self.__parking_spot_type = parking_spot_type
+
+    def is_free(self):
+        return self.__free
+
+    def assign_vehicle(self, vehicle):
+        self.__vehicle = vehicle
+        free = False
+
+    def remove_vehicle(self):
+        self.__vehicle = None
+        free = True
+
+
+class HandicappedSpot(ParkingSpot):
+    def __init__(self, number):
+        super().__init__(number, ParkingSpotType.HANDICAPPED)
+
+
+class CompactSpot(ParkingSpot):
+    def __init__(self, number):
+        super().__init__(number, ParkingSpotType.COMPACT)
+
+
+class LargeSpot(ParkingSpot):
+    def __init__(self, number):
+        super().__init__(number, ParkingSpotType.LARGE)
+
+
+class MotorbikeSpot(ParkingSpot):
+    def __init__(self, number):
+        super().__init__(number, ParkingSpotType.MOTORBIKE)
+
+
+class ElectricSpot(ParkingSpot):
+    def __init__(self, number):
+        super().__init__(number, ParkingSpotType.ELECTRIC)
